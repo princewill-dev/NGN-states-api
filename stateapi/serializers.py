ce77dd3f83504a0dc10ac8dce03e12716d966ff5
Princewill Ezekude <ezekudep@gmail.com>
2023-11-08 12:55:20 -0500

    LGAs for states added

--- stateapi/serializers.py
+++ stateapi/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import State
+from .models import LocalGovernment
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
+        fields = ['name']
+
+
+class LocalGovernmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LocalGovernment
         fields = ['name']
--- stateapi/views.py
+++ stateapi/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render, redirect
 from django.views import View
 from rest_framework import generics
 from .models import State
+from .models import LocalGovernment
 from .serializers import StateSerializer
+from .serializers import LocalGovernmentSerializer
+from django.http import Http404
 
 
 class HomePageView(View):
@@ -15,3 +18,15 @@ class HomePageView(View):
 class StateListView(generics.ListAPIView):
     queryset = State.objects.all()
     serializer_class = StateSerializer
+
+
+class LocalGovernmentListView(generics.ListAPIView):
+    serializer_class = LocalGovernmentSerializer
+
+    def get_queryset(self):
+        state_name = self.kwargs['state_name']
+        queryset = LocalGovernment.objects.filter(state__name=state_name)
+        if not queryset.exists():
+            raise Http404("Invalid state entered")
+        
+        return queryset
