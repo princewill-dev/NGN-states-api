7a03981a1efb9afbdf6a3068eb292cb8fa984b1a
Princewill Ezekude <ezekudep@gmail.com>
2023-11-11 13:35:16 -0500

    cors fix 002

--- NaijaProject/settings.py
+++ NaijaProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://nigerian-states-production.up.railway.app/",
+    "https://nigerian-states-production.up.railway.app",
 ]
 
 
