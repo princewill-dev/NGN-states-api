0594a688039a428c11968e48b4b7090e573ae8a4
Princewill Ezekude <ezekudep@gmail.com>
2023-11-18 18:55:26 -0500

    Update Dockerfile and settings.py

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 # Use specific Python version
-FROM python:latest
+FROM python:3.8-slim-buster
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1 
 
 # Set working directory
 WORKDIR /app 
+
+RUN apt-get update \
+  && apt-get install -y postgresql-client gcc python3-dev 
 
 # Copy the current directory contents into the container at /app
 COPY . /app
@@ -18,6 +24,12 @@ RUN pip install --no-cache-dir -r /app/requirements.txt
 
 # Collect static files
 RUN python /app/manage.py collectstatic --noinput
+
+ENV POSTGRES_DB=server_001
+ENV POSTGRES_USER=server_001
+ENV POSTGRES_PASSWORD=server_001
+
+ENV SECRET_KEY = 'django-insecure-zdjbo02g9pu)87h+c_o1fu_(yp()@cb9tu(cf1*ob-x^-u&pi-'
 
 # Expose ports
 EXPOSE 80 5432 8000
--- NaijaProject/settings.py
+++ NaijaProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zdjbo02g9pu)87h+c_o1fu_(yp()@cb9tu(cf1*ob-x^-u&pi-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -93,10 +93,22 @@ WSGI_APPLICATION = 'NaijaProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db', # database container name
+        'PORT': 5432,
     }
 }
 
